b0b3ad563c9ad0f5ef12656ee0683ca642dd2bc4
Bendelack <rickk1010@gmail.com>
2024-02-28 10:25:44 -0300

    teste pilha

--- pilha/Main.java
+++ pilha/Main.java
@@ -5,21 +5,28 @@ public class Main {
         try {
 
             Pilha pilha = new Pilha();
-            pilha.push(13);
-            pilha.push(13);
-            pilha.push(13);
-            pilha.push(13);
-            pilha.push(13);
-            pilha.push(13);
-            pilha.push(13);
-            pilha.push(13);
-            pilha.push(13);
-            pilha.push(13);
+            pilha.push(5);
+            pilha.push(3);
+            pilha.pop(); 
+            pilha.push(2); 
+            pilha.push(8);
+            pilha.pop(); 
+            pilha.pop(); 
+            pilha.push(9); 
+            pilha.push(1); 
+            pilha.pop(); 
+            pilha.push(7); 
+            pilha.push(6); 
+            pilha.pop(); 
+            pilha.pop();
+            pilha.push(4); 
+            pilha.pop();
+            pilha.pop();
             System.out.println(pilha.pop());
             System.out.println(pilha.size());
 
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (IsEmptyException err) {
+            System.out.println(err.getMessage());
         }
     }
     
