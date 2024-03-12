50bf64d445855de00541f5cb76f8a5d98efcf38c
Bendelack <rickk1010@gmail.com>
2024-03-12 09:12:29 -0300

    Update README.md

--- tad_pilha/Pilha.java
+++ tad_pilha/Pilha.java
@@ -4,21 +4,21 @@ public class Pilha {
     
     private int t;
     private int c;
-    private int[] p;
+    private Object[] p;
 
     public Pilha ( ) {
         this.t = -1;
         this.c = 8;
-        this.p = new int[8];
+        this.p = new Object[8];
     }
 
-    public void push ( int e ) {
+    public void push ( Object e ) {
         if ( (this.t + 1) == this.c )
             this.increaseCapacity();
         this.p[++this.t] = e;
     }
 
-    public int top ( ) throws IsEmptyException {
+    public Object top ( ) throws IsEmptyException {
         if ( this.t == -1 ) {
             IsEmptyException e;
             e = new IsEmptyException(this.t);
@@ -31,7 +31,7 @@ public class Pilha {
         return this.t + 1;
     }
 
-    public int pop ( ) throws IsEmptyException {
+    public Object pop ( ) throws IsEmptyException {
         if ( this.t == -1 ) {
             IsEmptyException e;
             e = new IsEmptyException(this.t);
@@ -42,7 +42,7 @@ public class Pilha {
     }
 
     private void increaseCapacity( ) {
-        int[] newArray = new int[2*this.c];
+        Object[] newArray = new Object[2*this.c];
         for ( int i = 0; i < this.c; i++ )
             newArray[i] = this.p[i];
         this.c = this.c*2;
@@ -50,7 +50,7 @@ public class Pilha {
     }
 
     public void empty ( ) {
-        int[] newArray = new int[this.c];
+        Object[] newArray = new int[this.c];
         this.p = newArray;
         this.t = -1;
     }
---
+++ tad_pilha/energia_solar/EnergiaSolar.java
@@ -0,0 +1,17 @@
+package tad_pilha.energia_solar;
+
+import java.util.Scanner;
+
+public class EnergiaSolar {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        scanner.close();
+
+    }
+
+}
